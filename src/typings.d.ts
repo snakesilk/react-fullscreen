d6d2913aabb7229c7e9feb676215b0e438da473a
Yoichiro Tanaka <yoichiro6642@gmail.com>
2020-07-20 06:06:09 +0900

    Fix issues about unit testing and eslint.

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,29 +3,30 @@
  * will be overridden with file-specific definitions by rollup
  */
 declare module '*.css' {
-  const content: { [className: string]: string };
-  export default content;
+  const content: { [className: string]: string }
+  export default content
 }
 
-interface SvgrComponent extends React.StatelessComponent<React.SVGAttributes<SVGElement>> {}
+interface SvgrComponent
+  extends React.StatelessComponent<React.SVGAttributes<SVGElement>> {}
 
 declare module '*.svg' {
-  const svgUrl: string;
-  const svgComponent: SvgrComponent;
-  export default svgUrl;
+  const svgUrl: string
+  const svgComponent: SvgrComponent
+  export default svgUrl
   export { svgComponent as ReactComponent }
 }
 
-declare module 'fscreen';
+declare module 'fscreen'
 
 interface FullScreenHandle {
-  active: boolean;
-  enter: () => void;
-  exit: () => void;
-  node: React.MutableRefObject<HTMLDivElement | null>;
+  active: boolean
+  enter: () => void
+  exit: () => void
+  node: React.MutableRefObject<HTMLDivElement | null>
 }
 
 interface FullScreenProps {
-  handle: FullScreenHandle;
-  onChange?: (state: boolean, handle: FullScreenHandle) => void;
+  handle: FullScreenHandle
+  onChange?: (state: boolean, handle: FullScreenHandle) => void
 }
