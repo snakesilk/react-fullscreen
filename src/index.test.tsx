b169fdb57778ffbb70d6c97f2a88274f8f9a4547
Pontus Persson <pontus.persson@einride.tech>
2020-12-18 17:00:13 +0100

    Expand test suite

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,4 +1,12 @@
-import { FullScreen, useFullScreenHandle } from '.';
+import fscreen from 'fscreen';
+import {
+  act,
+  renderHook,
+  RenderHookResult,
+} from '@testing-library/react-hooks';
+import { FullScreen, FullScreenHandle, useFullScreenHandle } from '.';
+
+jest.mock('fscreen');
 
 describe('exports', () => {
   it('exports component', () => {
@@ -7,5 +15,110 @@ describe('exports', () => {
 
   it('exports hook', () => {
     expect(useFullScreenHandle).toBeTruthy();
+  });
+});
+
+describe('useFullScreenHandle', () => {
+  let hook: RenderHookResult<unknown, FullScreenHandle>;
+  const mockNode = Symbol('fake node');
+
+  beforeEach(() => {
+    fscreen.addEventListener = jest.fn();
+    fscreen.exitFullscreen = jest.fn(() => Promise.resolve());
+    fscreen.requestFullscreen = jest.fn(() => Promise.resolve());
+  });
+
+  describe('when mounted', () => {
+    let listener: () => void;
+    beforeEach(() => {
+      hook = renderHook(() => useFullScreenHandle());
+      hook.result.current.node.current = (mockNode as unknown) as HTMLDivElement;
+      listener = fscreen.addEventListener.mock.calls[0][1];
+    });
+
+    it('has active flag set to false', () => {
+      expect(hook.result.current.active).toBe(false);
+    });
+
+    it('listens to fullscreen change', () => {
+      expect(fscreen.addEventListener).toHaveBeenCalledTimes(1);
+    });
+
+    describe('and fullscreen event fires on current handle', () => {
+      beforeEach(() => {
+        fscreen.fullscreenElement = mockNode;
+        act(() => {
+          listener();
+        });
+      });
+
+      it('active flag is set to true', () => {
+        expect(hook.result.current.active).toBe(true);
+      });
+    });
+
+    describe('and fullscreen event fires on other handle', () => {
+      beforeEach(() => {
+        fscreen.fullscreenElement = Symbol('other fake node');
+        act(() => {
+          listener();
+        });
+      });
+
+      it('active flag is set to false', () => {
+        expect(hook.result.current.active).toBe(false);
+      });
+    });
+
+    describe('and enter() called', () => {
+      beforeEach(async () => {
+        await act(async () => {
+          hook.result.current.enter();
+        });
+      });
+
+      it('calls fscreen.requestFullscreen with node', () => {
+        expect(fscreen.requestFullscreen).toHaveBeenCalledTimes(1);
+        expect(fscreen.requestFullscreen).toHaveBeenCalledWith(mockNode);
+      });
+    });
+
+    describe('when entered', () => {
+      beforeEach(() => {
+        fscreen.fullscreenElement = mockNode;
+      });
+
+      describe('and enter() called', () => {
+        beforeEach(async () => {
+          await act(async () => {
+            hook.result.current.enter();
+          });
+        });
+
+        it('calls fscreen.exitFullscreen', () => {
+          expect(fscreen.exitFullscreen).toHaveBeenCalledTimes(1);
+        });
+
+        it('calls fscreen.requestFullscreen', () => {
+          expect(fscreen.requestFullscreen).toHaveBeenCalledTimes(1);
+        });
+      });
+
+      describe('and exit() called', () => {
+        beforeEach(async () => {
+          await act(async () => {
+            hook.result.current.exit();
+          });
+        });
+
+        it('calls fscreen.exitFullscreen', () => {
+          expect(fscreen.exitFullscreen).toHaveBeenCalledTimes(1);
+        });
+
+        it('does not call fscreen.requestFullscreen', () => {
+          expect(fscreen.requestFullscreen).toHaveBeenCalledTimes(0);
+        });
+      });
+    });
   });
 });
