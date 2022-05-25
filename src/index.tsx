362accf461a1d117573c92e8b4078cfdfcb86eba
Mark Hayward <markhayward@live.com>
2022-05-25 09:05:01 -0600

    Added children: any; to FullScreenProps interface

--- src/index.tsx
+++ src/index.tsx
@@ -16,6 +16,7 @@ export interface FullScreenHandle {
 
 export interface FullScreenProps {
   handle: FullScreenHandle;
+  children: any;
   onChange?: (state: boolean, handle: FullScreenHandle) => void;
   className?: string;
 }
